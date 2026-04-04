38a9c6dc53d358e9d69f5fa10e15abc83cc04b48
Chinmay Joshi <joshichinmay2006@gmail.com>
2026-04-05 04:25:58 +0530

    Chinmay Phase 1: folder structure and main.cpp created

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "EventSimEngine project is set up successfully." << std::endl;
+  std::cout << "This message will be replaced when the full project is ready." << std::endl;
+  return 0;
+}
